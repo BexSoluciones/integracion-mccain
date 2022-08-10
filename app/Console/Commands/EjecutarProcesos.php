1b3dd85bdbd4698a38b8dcd7b69f8f905b65987d
DevJoseMarin <jose.dev.web@gmail.com>
2022-08-09 23:36:52 -0500

    actualizar ejecucion planos

--- app/Console/Commands/EjecutarProcesos.php
+++ app/Console/Commands/EjecutarProcesos.php
@@ -33,7 +33,7 @@ class EjecutarProcesos extends Command
 
         Artisan::call('integracion:guardar-informacion'); // VERIFICA INVENTARIO MINIMO EN BODEGAS PARA REMISIONAR, MOVER INVENTARIO
 
-        Artisan::call('integracion:generar-planos'); // VERIFICA INVENTARIO MINIMO EN BODEGAS PARA REMISIONAR, MOVER INVENTARIO
+        // Artisan::call('integracion:generar-planos'); // VERIFICA INVENTARIO MINIMO EN BODEGAS PARA REMISIONAR, MOVER INVENTARIO
         
         // Artisan::call('integracion:enviar-planos'); // VERIFICA INVENTARIO MINIMO EN BODEGAS PARA REMISIONAR, MOVER INVENTARIO
         
